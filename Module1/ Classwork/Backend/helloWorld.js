1de70359a6063eb7c90e395e65fd2e3454175b7e
GNA-Gavin <gavin.n.aves@gmail.com>
2025-02-12 21:01:36 +1300

    update file structure

---
+++ Module1/ Classwork/Backend/helloWorld.js
@@ -0,0 +1,15 @@
+function showMessages(){
+    console.log("Hello World");
+    console.log("Hello World 2");
+    console.log("It's Gavin");
+    console.log("Goodbye!!!");
+}
+
+showMessages();
+
+function sum(a, b){
+    console.log(a+b)
+}
+
+sum(12, 7);
+sum(123, 234);
