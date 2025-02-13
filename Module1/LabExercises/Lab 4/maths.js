c94eda0f98d2c05b832f0a9f7aa8bbe580ddb7e9
GNA-Gavin <gavin.n.aves@gmail.com>
2025-02-13 13:12:01 +1300

    lab 4

---
+++ Module1/LabExercises/Lab 4/maths.js
@@ -0,0 +1,25 @@
+function minus(a, b) {
+  return a - b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function helloName(a){
+    console.log("Hello "+ a);
+}
+
+console.log(minus(10,5));
+console.log(add(10,5));
+console.log(divide(10,5));
+console.log(multiply(10,5));
+helloName("Mirza");
